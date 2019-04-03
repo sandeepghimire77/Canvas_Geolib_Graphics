282ed75f2f056509002a75b1aba099de8b12d101
Sandeep Ghimire <s.ghimire@laserhub.de>
2019-04-03 21:33:08 +0200

    starting app

--- geloib/canvas-script.js
+++ geloib/canvas-script.js
@@ -1,10 +1,11 @@
 // Definitions
 var canvas = document.getElementById("hello-world-canvas"); // handle the canvas
-var context = canvas.getContext("2d"); // handle the context 
+var context = canvas.getContext("2d"); // handle the context .. we have 3 dimensional context too but here we use 2d context
+// these are the core object that we will use in everywhere .
 
 // Blue rectangle
 context.fillStyle = "blue";
-context.fillRect(10,40,30,70);
+context.fillRect(15,45,30,70);
 
 // Yellow rectangle
 context.fillStyle = "yellow";
