b2c40d379d3b6c5e0af73ebcbb110681164dc436
Sandeep Ghimire <s.ghimire@laserhub.de>
2019-04-03 21:40:57 +0200

    line draw

--- geloib/canvas-script.js
+++ geloib/canvas-script.js
@@ -1,12 +1,17 @@
-// Definitions
-var canvas = document.getElementById("hello-world-canvas"); // handle the canvas
-var context = canvas.getContext("2d"); // handle the context .. we have 3 dimensional context too but here we use 2d context
-// these are the core object that we will use in everywhere .
 
-// Blue rectangle
-context.fillStyle = "blue";
-context.fillRect(15,45,30,70);
+window.onload = function () {
 
-// Yellow rectangle
-context.fillStyle = "yellow";
-context.fillRect(50,30,60,30);
+  // Definitions
+  var canvas = document.getElementById("line-canvas");
+  var context = canvas.getContext("2d");
+
+  // Draw line steps
+  context.beginPath(); // reset the context state
+  context.strokeStyle = "red"; // color of the line
+  context.lineWidth = 10; // thickness of the line
+  context.moveTo(30, 70); // moveTo(x,y) -> starting point of the line
+  context.lineTo(130, 70); // line(x,y) -> end point of the line
+  context.stroke(); // draws the line
+
+
+}
